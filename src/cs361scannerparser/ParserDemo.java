609614677597f6f9dcd5dc63863ae7b1c6b1eef4
qqwwsyz <zc86534p@pace.edu>
2020-11-05 02:50:06 +0800

    initial

---
+++ src/cs361scannerparser/ParserDemo.java
@@ -0,0 +1,17 @@
+package cs361scannerparser;
+
+public class ParserDemo {
+
+	public static void main(String[] args) {
+
+		// Change the path!
+		TokenStream tStream = new TokenStream("C:\\\\Users\\\\cscharff\\\\eclipse-workspace\\\\ScannerSolution\\\\src\\\\prog1.jay");
+
+		ConcreteSyntax cSyntax = new ConcreteSyntax(tStream);
+		Program p = cSyntax.program();
+		System.out.println(p.display());
+		System.out.println("test");
+
+	}
+
+}
