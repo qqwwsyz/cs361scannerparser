293927d856a05e1b701546b34e6bd1bd7d6ea24a
qqwwsyz <zc86534p@pace.edu>
2020-11-10 03:11:30 +0800

    add my own tester

--- src/cs361scannerparser/ScannerDemo.java
+++ src/cs361scannerparser/ScannerDemo.java
@@ -1,18 +1,18 @@
 package cs361scannerparser;
 /**
  * @author scharffc
- * 
- */
+ *  ZHEMING AND GUO COMPLETED
+ */ 
 public class ScannerDemo {
 
-	private static String file1 = "C:\\Users\\ZHEMING\\eclipse-workspace\\cs361scannerparser\\prog1.jay";
+	private static String file1 = "C:\\Users\\ZHEMING\\eclipse-workspace\\cs361scannerparser\\owntester.jay";
 	private static int counter = 1;
 	private static Token tk;
 	public static void main(String args[]) {
 		TokenStream ts = new TokenStream(file1);
 		System.out.println(file1);
 		while (!ts.isEndofFile()) {
-			// TO BE COMPLETED
+			// COMPLETED
 			while (!ts.isEndofFile()) {
 				tk = ts.nextToken();
 				
